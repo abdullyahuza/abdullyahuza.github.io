07a0bab959fdad3d044e6af7cc0eafade67618ec
abdullyahuza <yahuzaabdulrazak@gmail.com>
2024-05-13 13:38:49 +0100

    feat: add quoates

--- random-quotes-machine/script.js
+++ random-quotes-machine/script.js
@@ -3,6 +3,21 @@ const author = document.getElementById("author")
 const tweetButton = document.getElementById("tweet-quote")
 const quote = []
 
+const quotes = [
+    'The best teacher is experience learned from failures.',
+    'In all chaos there is a cosmos, in all disorder a secret order.',
+    'He who angers you conquers you.',
+    'Good luck is another name for tenacity of purpose.',
+    'Believe you can and you are halfway there.',
+    "It always seems impossible until it's done",
+    "Don't watch the clock; do what it does, keep going.",
+    "Keep going. The only way to guarantee failure is to stop",
+    "You are stronger than you seem, braver than you believe, and smarter than you think.",
+    "The only way to do great work is to love what you do.",
+    "You got this! just take it one step at a time",
+    "Without commitment you will never start, and more importantly without consistency you will never finish!"
+];
+
 function getQuote(){
     // const data
     fetch('https://type.fit/api/quotes')
